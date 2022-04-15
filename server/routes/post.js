d5dd9c93a8d901a47936f1fc82c3fffdf973d14b
unknown <raisulkhairi20@gmail.com>
2022-04-15 15:03:30 +0700

    update folder

---
+++ server/routes/post.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+// import controller methods
+const { create, list, read, update, remove } = require('../controllers/post');
+
+router.post('/post', create);
+router.get('/posts', list);
+router.get('/post/:slug', read);
+router.put('/post/:slug', update);
+router.delete('/post/:slug', remove);
+
+module.exports = router;
